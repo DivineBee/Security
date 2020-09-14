8cb5769e3979c8176eccf17421e574ed8c94e567
Beatrix <49019844+DivineBee@users.noreply.github.com>
2020-09-14 12:20:59 +0300

    update gui

--- gui/gui.py
+++ gui/gui.py
@@ -1,13 +1,16 @@
+import glob
+import tarfile
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog as fd
+import requests
 import view_audit_structure
 
 global previous
 
 main = Tk()
 main.title("Controls Choice List")
-main.geometry("1250x475")
+main.geometry("1620x575")
 frame = ttk.Frame(main, padding=(4, 4, 16, 16))
 frame.grid(column=0, row=0)
 previous = []
@@ -57,8 +60,8 @@ lstbox.bind('<<ListboxSelect>>', on_select_configuration)
 # Saving file with desired configurations
 def save_config():
     file_name = fd.asksaveasfilename(filetypes=(("Audit FILES", ".audit"),
-                                                ("Read files", ".txt"),
                                                 ("All files", ".")))
+    file_name += '.audit'
     file = open(file_name, 'w')
     selection = lstbox.curselection()
     for i in selection:
@@ -73,9 +76,25 @@ def select_all():
         lstbox.insert(END, struct)
 
 
-def diselect_all():
+def deselect_all():
     for struct in structure:
         lstbox.selection_clear(0, END)
+
+
+def download_url(url, save_path, chunk_size=1024):
+    r = requests.get(url, stream=True)
+    with open(save_path, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fd.write(chunk)
+
+
+def extract_download():
+    url="https://www.tenable.com/downloads/api/v1/public/pages/download-all-compliance-audit-files/downloads/7472/download?i_agree_to_tenable_license_agreement=true"
+    path="audits.tar.gz"
+    download_url(url,path)
+    tf = tarfile.open("audits.tar.gz")
+    tf.extractall()
+    print(glob.glob("portal_audits/*"))
 
 
 text = Text(frame, bg="#fffffa", width=50, height=27.5)
@@ -83,6 +102,7 @@ text.grid(row=0, column=3, columnspan=3, padx=30)
 import_button = Button(frame, text="Import", width=7, height=1, command=import_audit).place(x=10, y=510)  # y was 435
 openButton = Button(frame, text="Save", width=7, height=1, command=save_config).place(x=80, y=510)
 selectAllButton = Button(frame, text="Select All", width=7, height=1, command=select_all).place(x=150, y=510)
-diselectAllButton = Button(frame, text="Diselect All", width=7, height=1, command=diselect_all).place(x=220, y=510)
+deselectAllButton = Button(frame, text="Deselect All", width=10, height=1, command=deselect_all).place(x=220, y=510)
+downloadButton = Button(frame, text="Download audits", width=15, height=1, command=extract_download).place(x=310, y=510)
 
 main.mainloop()
